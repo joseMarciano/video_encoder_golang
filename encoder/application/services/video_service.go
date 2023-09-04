e29c56bcfd0650dac35da434a44d41e02c46001c
joseMarciano <marcianojosepaulo@gmail.com>
2023-09-03 21:20:43 -0300

    feat: :sparkles: create a function to fragment video using bento4

--- encoder/application/services/video_service.go
+++ encoder/application/services/video_service.go
@@ -3,14 +3,20 @@ package services
 import (
 	"encoder/application/repository"
 	"encoder/domain"
+	"fmt"
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/credentials"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3"
-	"io"
+	"io/ioutil"
+	"log"
 	"os"
-	"path"
+	"os/exec"
 )
+
+const localStoragePath = "local-path-enconder"
+const MP4 = ".mp4"
+const FRAG = ".frag"
 
 type VideoService struct {
 	Video           *domain.Video
@@ -24,6 +30,15 @@ func NewVideoService(video *domain.Video) VideoService {
 }
 
 func (this *VideoService) Download(bucketName string) error {
+	directory := localStoragePath + "/" + this.Video.ID
+	err := os.MkdirAll(directory, os.ModePerm)
+	if err != nil {
+		log.Fatalf("error on Mkdir %s", err.Error())
+		return err
+	}
+
+	get, _ := credentials.NewEnvCredentials().Get()
+	fmt.Printf("Credentilas %s - %s ", get.AccessKeyID, get.SecretAccessKey)
 	sess, err := session.NewSession(&aws.Config{
 		Region:      aws.String("us-east-1"),
 		Credentials: credentials.NewEnvCredentials(),
@@ -44,23 +59,41 @@ func (this *VideoService) Download(bucketName string) error {
 		return err
 	}
 
-	content, err := io.ReadAll(object.Body)
+	content, err := ioutil.ReadAll(object.Body)
 
 	if err != nil {
 		return err
 	}
 
-	file, err := os.Create(path.Base("") + "/.local-path-enconder" + this.Video.ID + ".mp4")
+	err = ioutil.WriteFile(directory+"/"+this.Video.ID+".mp4", content, os.ModePerm)
 	if err != nil {
 		return err
 	}
 
-	_, err = file.Write(content)
-	if err != nil {
-		return err
-	}
-
-	defer file.Close()
 	return nil
 
+}
+
+func (this *VideoService) Fragment() error {
+	directory := localStoragePath + "/" + this.Video.ID + "/" + this.Video.ID
+	source := directory + MP4
+	target := directory + FRAG
+
+	cmd := exec.Command("mp4fragment", source, target)
+	output, err := cmd.CombinedOutput()
+
+	if err != nil {
+		log.Fatalf("error on CombinedOutput %s", err.Error())
+		return err
+	}
+
+	printOutput(output)
+
+	return nil
+}
+
+func printOutput(out []byte) {
+	if len(out) > 0 {
+		log.Printf("===> Output %s\n", string(out))
+	}
 }
